0491701e78e40b3cf06137882b7342b41a46c07b
KamranSattary <kamran.nejad-sattary@epfl.ch>
2020-12-06 19:31:21 +0100

    added roc plot which works for svm, use this please

--- src/helpers.py
+++ src/helpers.py
@@ -1,5 +1,8 @@
 import re 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import ensemble, pipeline, model_selection, metrics, preprocessing, svm
 
 def load_data(data_path='../data/', muchlinski_data=True, fl_data=True, ch_data=True, hs_data=True):
     
@@ -41,3 +44,63 @@ def load_data(data_path='../data/', muchlinski_data=True, fl_data=True, ch_data=
         data['muchlinski'] = (muchlinski_x, y_master)
     
     return data
+
+def roc_plt(X, y, pipe, title, k_fold=5, seed=0):
+    
+    # set seed and ensure input are numpy arrays
+    np.random.seed(seed)
+    X, y = X.to_numpy(), y.to_numpy()
+    cv = model_selection.StratifiedKFold(n_splits=k_fold)
+
+    # initiate empty lists to store fold scoring to aggregate later
+    tprs, aucs = [], []
+    
+    # x array used to interpolate tprs from the fpr and tpr returned by plot_roc_curve to later determine means over folds
+    mean_fpr = np.linspace(0, 1, 100)
+
+    # define subplot and loop through folds retrieving scores on test folds
+    fig, ax = plt.subplots(figsize=(12,8))
+    for i, (train, test) in enumerate(cv.split(X, y)):
+        
+        # fit model
+        pipe.fit(X[train], y[train])
+
+        # plot roc-score on each test fold
+        viz = metrics.plot_roc_curve(pipe, X[test], y[test], 
+                                     name='ROC fold {}'.format(i), alpha=0.3, lw=1, ax=ax)
+        
+        # create scoring backlog of scores, and interpolated tprs for determining means over folds
+        interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
+        interp_tpr[0] = 0.0
+        tprs.append(interp_tpr) ; aucs.append(viz.roc_auc)
+
+    # red diagonal representing "chance" of ROC-AUC plot - predicting unrelated to the true label
+    ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+            label='Chance', alpha=.8)
+
+    # add mean line to plot
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = metrics.auc(mean_fpr, mean_tpr)
+    std_auc = np.std(aucs)
+    
+    ax.plot(mean_fpr, mean_tpr, color='b',
+            label=r'Mean ROC (AUC = %0.4f $\pm$ %0.4f)' % (mean_auc, std_auc),
+            lw=2, alpha=.8)
+
+    # add standard deviation from mean line
+    std_tpr = np.std(tprs, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2, label=r'$\pm$ 1 std. dev.')
+    
+    # set limits, title & legend and display plot used in this training process
+    ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05], title=title)
+    ax.legend(bbox_to_anchor=(1.05, 1))
+    
+    plt.tight_layout()
+    plt.show()
+    
+    dict_vars = dict(mean_fpr=mean_fpr, mean_tpr=mean_tpr, mean_auc=mean_auc)
+    
+    return dict_vars
